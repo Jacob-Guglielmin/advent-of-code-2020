0368f481bc237a285d5050a6970532955803e876
Jacob-Guglielmin <jacobguglielmin@gmail.com>
2020-12-05 08:14:43 -0700

    Day 5 improvement

--- Day 5/part1.js
+++ Day 5/part1.js
@@ -4,11 +4,8 @@ var input = fs.readFileSync("input.txt").toString().split("\n");
 
 var highestID = 0;
 
-var binary;
-
 for (i of input) {
-    binary = i.replace(/L|F/g, "0").replace(/B|R/g, "1");
-    var id = parseInt(binary.substring(0, 7), 2) * 8 + parseInt(binary.substring(7, 10), 2);
+    var id = parseInt(i.replace(/L|F/g, "0").replace(/B|R/g, "1"), 2);
     if (id > highestID) {
         highestID = id;
     }
--- Day 5/part2.js
+++ Day 5/part2.js
@@ -7,8 +7,7 @@ var idList = [];
 var binary;
 
 for (i of input) {
-    binary = i.replace(/L|F/g, "0").replace(/B|R/g, "1");
-    idList.push(parseInt(binary.substring(0, 7), 2) * 8 + parseInt(binary.substring(7, 10), 2));
+    idList.push(parseInt(i.replace(/L|F/g, "0").replace(/B|R/g, "1"), 2));
 }
 
 idList.sort(function(a, b){return a-b});
